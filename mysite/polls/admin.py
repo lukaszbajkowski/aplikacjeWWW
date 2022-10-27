c14471456c2f03f940784c6b59b86d5428e86157
Lukasz Bajkowki <bajkowski1e@gmail.com>
2022-10-27 17:17:07 +0200

    Stan na 27.10.2022

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Question, Choice, Value
+from .models import Question, Choice, Value, Person, Osoba
 
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(Value)
+admin.site.register(Person)
+admin.site.register(Osoba)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -30,3 +30,36 @@ class Value(models.Model):
 
     def __str__(self):
         return self.color + " " + str(self.value)
+
+
+class Person(models.Model):
+    # lista wartości do wyboru w formie krotek
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    # wskazanie listy poprzez przypisanie do parametru choices
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+
+class Osoba(models.Model):
+    MONTH = (
+        ('1', 'Styczeń'),
+        ('2', 'Luty'),
+        ('3', 'Marzec'),
+        ('4', 'Kwiecień'),
+        ('5', 'Maj'),
+        ('6', 'Czerwiec'),
+        ('7', 'Lipiec'),
+        ('8', 'Sierpień'),
+        ('9', 'Wrzesień'),
+        ('10', 'Październik'),
+        ('11', 'Listopad'),
+        ('12', 'Grudzień'),
+    )
+    imie = models.CharField(max_length=20, null=False, blank=False)
+    nazwisko = models.CharField(max_length=20, null=False, blank=False)
+    miesiac_urodzenia = models.CharField(default=1, max_length=2, choices=MONTH)
+    data_dodania = models.DateField(auto_now=True)
