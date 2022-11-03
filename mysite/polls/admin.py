f78cd518dd4618fa8926fae7db70dbb40e1278c5
Lukasz Bajkowki <bajkowski1e@gmail.com>
2022-11-03 17:21:02 +0100

    Stan na 03.11.2022

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import Question, Choice, Value, Person, Osoba
+from .models import Question, Choice, Value, Person, Team
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'nazwisko', 'shirt_size', 'team']
+
+class DruzynaAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country']
+
 
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(Value)
-admin.site.register(Person)
-admin.site.register(Osoba)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Team, DruzynaAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,28 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinLengthValidator
+
+SHIRT_SIZES = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+)
+
+MONTH = (
+    (1, 'Styczeń'),
+    (2, 'Luty'),
+    (3, 'Marzec'),
+    (4, 'Kwiecień'),
+    (5, 'Maj'),
+    (6, 'Czerwiec'),
+    (7, 'Lipiec'),
+    (8, 'Sierpień'),
+    (9, 'Wrzesień'),
+    (10, 'Październik'),
+    (11, 'Listopad'),
+    (12, 'Grudzień'),
+)
 
 
 class Question(models.Model):
@@ -32,34 +54,30 @@ class Value(models.Model):
         return self.color + " " + str(self.value)
 
 
+class Team(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(validators=[MinLengthValidator(2)], max_length=2, blank=False, null=False)
+
+    def save(self, *args, **kwargs):
+        self.country = self.country.upper()
+        return super(Team, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
 class Person(models.Model):
-    # lista wartości do wyboru w formie krotek
-    SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
-    name = models.CharField(max_length=60)
-    # wskazanie listy poprzez przypisanie do parametru choices
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
-
-
-class Osoba(models.Model):
-    MONTH = (
-        ('1', 'Styczeń'),
-        ('2', 'Luty'),
-        ('3', 'Marzec'),
-        ('4', 'Kwiecień'),
-        ('5', 'Maj'),
-        ('6', 'Czerwiec'),
-        ('7', 'Lipiec'),
-        ('8', 'Sierpień'),
-        ('9', 'Wrzesień'),
-        ('10', 'Październik'),
-        ('11', 'Listopad'),
-        ('12', 'Grudzień'),
-    )
-    imie = models.CharField(max_length=20, null=False, blank=False)
+    first_name = models.CharField(max_length=20, null=False, blank=False)
     nazwisko = models.CharField(max_length=20, null=False, blank=False)
-    miesiac_urodzenia = models.CharField(default=1, max_length=2, choices=MONTH)
-    data_dodania = models.DateField(auto_now=True)
+    miesiac_urodzenia = models.IntegerField(default=1, choices=MONTH)
+    data_dodania = models.DateField(auto_now_add=True)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="Drużyna")
+
+    def __str__(self):
+        return self.first_name + " " + self.nazwisko
+
+    class Meta:
+        ordering = ["nazwisko"]
+
+
