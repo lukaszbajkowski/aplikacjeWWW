fbb9caf66865b5dbb9516659f03273462625184b
mixvert <bajkkowski1e@gmail.com>
2022-11-17 16:48:21 +0100

    Stan na 17.11.2022

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('persons/', views.person_list),
     path('persons/<int:pk>/', views.person_detail),
+    path('teams/', views.team_list),
+    path('teams/<int:pk>/', views.team_detail),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,36 +4,32 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Person, Team
-from .serializers import PersonSerializer
+from .serializers import PersonSerializer, TeamSerializer
 
 
 @api_view(['GET'])
 def person_list(request):
-    """
-    Lista wszystkich obiektów modelu Person.
-    """
     if request.method == 'GET':
         persons = Person.objects.all()
         serializer = PersonSerializer(persons, many=True)
         return Response(serializer.data)
 
 
+@api_view(['GET'])
+def person_list(request):
+    if request.method == 'GET':
+        persons_list = Person.objects.filter(first_name__contains = 'a')
+        serializer = PersonSerializer(persons_list, many=True)
+        return Response(serializer.data)
+
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def person_detail(request, pk):
-
-    """
-    :param request: obiekt DRF Request
-    :param pk: id obiektu Person
-    :return: Response (with status and/or object/s data)
-    """
     try:
         person = Person.objects.get(pk=pk)
     except Person.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    """
-    Zwraca pojedynczy obiekt typu Person.
-    """
     if request.method == 'GET':
         person = Person.objects.get(pk=pk)
         serializer = PersonSerializer(person)
@@ -48,4 +44,36 @@ def person_detail(request, pk):
 
     elif request.method == 'DELETE':
         person.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def team_list(request):
+    if request.method == 'GET':
+        teams = Team.objects.all()
+        serializer = TeamSerializer(teams, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def team_detail(request, pk):
+    try:
+        team = Team.objects.get(pk=pk)
+    except Team.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        team = Team.objects.get(pk=pk)
+        serializer = TeamSerializer(team)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TeamSerializer(team, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        team.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
